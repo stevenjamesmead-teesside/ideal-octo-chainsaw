2ddf991627c6033105945ca68f90943b5f577ef1
u0012604 <steven.j.mead@tees.ac.uk>
2023-02-07 16:08:23 +0000

    Initial Commit

---
+++ app/src/main/java/uk/ac/tees/u0012604/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.u0012604;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
